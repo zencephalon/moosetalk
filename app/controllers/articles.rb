e5e200b4543c7e2bb8681afe63367c204768248e
Matthew Bunday <zenceph@gmail.com>
2015-01-13 15:14:08 -0500

    Add article controller.

---
+++ app/controllers/articles.rb
@@ -0,0 +1,37 @@
+# Index
+get '/article/all' do
+  @articles = Article.all
+  erb :'article/index'
+end
+
+# New article
+get '/article/new' do
+  erb :'article/new'
+end
+
+post '/article' do
+  article = Article.create(params[:article])
+  redirect ("/article/#{article.id}")
+end
+
+# Existing article
+get '/article/:id' do |id|
+  @article = Article.find(id)
+  erb :'article/show', locals: {article: @article}
+end
+
+get '/article/:id/edit' do |id|
+  @article = Article.find(id)
+  erb :'article/edit'
+end
+
+put '/article/:id' do |id|
+  article = Article.find(id)
+  article.update(params[:article])
+  redirect ("/article/#{article.id}")
+end
+
+delete '/article/:id' do |id|
+  Article.find(id).destroy
+  redirect "/article/all"
+end
