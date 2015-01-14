58db6d483f172aa9c5410651507dd27ade4e1b7d
Matthew Bunday <zenceph@gmail.com>
2015-01-14 11:31:57 -0500

    Associate users with messages.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,9 @@ class User < ActiveRecord::Base
 
   has_many :commented_articles, through: :comments, source: :article
 
+  has_many :sent_messages, foreign_key: "sender_id", class_name: "Message"
+  has_many :received_messages, foreign_key: "receiver_id", class_name: "Message"
+
+  has_many :senders, through: :received_messages, source: :sender
+  has_many :receivers, through: :sent_messages, source: :receiver
 end
