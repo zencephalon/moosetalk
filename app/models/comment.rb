60063a8e31e027bf17521a4dc05e6608087d979c
Matthew Bunday <zenceph@gmail.com>
2015-01-15 16:07:29 -0500

    Add validations to comment model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :article
   belongs_to :user
+
+  validates :user, presence: true
+  validates :title, presence: true
+  validates :content, presence: true
 end
