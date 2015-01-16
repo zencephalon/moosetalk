3f7ac3831efbd53ca611ca05ec8978101ec0bfda
Matthew Bunday <zenceph@gmail.com>
2015-01-16 10:21:59 -0500

    Setup models for many to many between articles and tags.

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,9 @@
 class Article < ActiveRecord::Base
   has_many :comments
   belongs_to :user
+
+  has_many :article_tags
+  has_many :tags, through: :article_tags
   #belongs_to :user, foreign_key: :user_id
   #belongs_to :user, foreign_key: :author_id
 end
