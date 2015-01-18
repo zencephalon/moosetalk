97ddc83a4097dbd21d7745a05a1a9d06e9f0e7fe
Matthew Bunday <zenceph@gmail.com>
2015-01-18 17:36:20 -0500

    Handle tag functionality.

--- app/controllers/articles.rb
+++ app/controllers/articles.rb
@@ -15,6 +15,7 @@ end
 
 post '/article' do
   article = Article.create(params[:article])
+  article.parse_tags!(params[:tags])
   redirect ("/article/#{article.id}")
 end
 
@@ -32,6 +33,7 @@ end
 put '/article/:id' do |id|
   article = Article.find(id)
   article.update(params[:article])
+  article.parse_tags!(params[:tags])
   redirect ("/article/#{article.id}")
 end
 
--- app/models/article.rb
+++ app/models/article.rb
@@ -6,4 +6,15 @@ class Article < ActiveRecord::Base
   has_many :tags, through: :article_tags
   #belongs_to :user, foreign_key: :user_id
   #belongs_to :user, foreign_key: :author_id
+  def parse_tags!(tags_str)
+    self.tags.clear
+    tags_str.split(',').map(&:strip).uniq.each do |tag_str|
+      tag = Tag.where(name: tag_str).first_or_create
+      self.tags << tag
+    end
+  end
+
+  def tags_str
+    self.tags.map(&:name).join(", ")
+  end
 end
