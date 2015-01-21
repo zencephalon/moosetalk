d85e85e8a761a2c797e415a301da9516da0002d4
Matthew Bunday <zenceph@gmail.com>
2015-01-21 09:28:01 -0500

    Add reply associations to comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,7 @@ class Comment < ActiveRecord::Base
   validates :user, presence: true
   validates :title, presence: true
   validates :content, presence: true
+
+  has_many :replies, foreign_key: :parent_id, class_name: "Comment"
+  belongs_to :parent, foreign_key: :parent_id, class_name: "Comment"
 end
